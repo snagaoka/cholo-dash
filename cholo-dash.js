eb8865e8043019d4a1b152c910bba23efa6e69ff
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-28 10:34:02 -1000

    adds description to compact function

--- cholo-dash.js
+++ cholo-dash.js
@@ -1,7 +1,11 @@
 // utility functions
 _ = {};
 
+/*
+Creates an array with all falsey values removed. 
+The values false, null, 0, "", undefined, and NaN are all falsey.
+*/
 
 _.compact = function (ar) {
-	
+
 }
