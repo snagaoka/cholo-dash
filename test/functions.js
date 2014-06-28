2e862b3e2ce230bcabaea0d90373bfb8284f1598
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-28 11:03:34 -1000

    tests added with JSON file

---
+++ test/functions.js
@@ -0,0 +1,21 @@
+var chai = require('chai'),
+  should = chai.shoul,
+  _ = require('../cholo-dash.js');
+
+describe("Array functions", function(){
+
+
+  it("should compact an array", function(){
+    var expected = [1, 2, 3];
+    var input = [0, 1, , false, 2, '', 3];
+    var result = _.compact(input);
+    result.should.include(1);
+    result.should.include(2);
+    result.should.include(3)
+    result.should.not.include(0);
+    result.should.not.include(false);
+    result.should.not.include('');
+
+  });
+
+});
