7df66f0cb36c8464834941230a0b952bbbb32ba6
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-28 11:56:46 -1000

    difference test added

--- test/functions.js
+++ test/functions.js
@@ -1,5 +1,5 @@
 var chai = require('chai'),
-  should = chai.shoul,
+  should = chai.should(),
   _ = require('../cholo-dash.js');
 
 describe("Array functions", function(){
@@ -17,5 +17,20 @@ describe("Array functions", function(){
     result.should.not.include('');
 
   });
+
+  it("should identify the difference between two different arrays", function(){
+    var expected = [1, 3, 4];
+    var input1 = [1, 2, 3, 4, 5]; 
+    var input2 [5, 2, 10];
+    var result = _.difference(input)
+    result.should.include(1);
+    result.should.include(3);
+    result.should.include(4);
+    result.should.not.include(2);
+    result.should.not.include(5);
+    result.should.not.include(10);
+
+
+});
 
 });
