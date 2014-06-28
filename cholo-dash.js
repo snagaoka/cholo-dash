3054249f38e90c00e31ff1e9f7b644199b0934f6
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-28 13:54:13 -1000

    worked on difference function

--- cholo-dash.js
+++ cholo-dash.js
@@ -14,4 +14,18 @@ _.compact = function (ar) {
 	return ar;
 }
 
+/*
+Creates an array excluding all values of the provided arrays using strict equality for comparisons, i.e. ===.
+*/
+_.difference = function (ar, values) {
+	for (var i = ar.length - 1; i >= 0; i--) {
+		for (var j = values.length - 1; j >= 0; j--) {
+			if(ar[i] === values[j]) {
+				ar.splice(i, 1);
+			};
+		};
+	};
+	return ar;
+}
+
 module.exports = _;
