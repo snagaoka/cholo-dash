e3755ed50d291498101ac3646c68981e0446bf07
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-28 16:16:15 -1000

    started findLastIndex function

--- cholo-dash.js
+++ cholo-dash.js
@@ -34,11 +34,26 @@ If a property name is provided for callback the created "_.pluck" style callback
 If an object is provided for callback the created "_.where" style callback will return true for elements that have the properties of the given object, else false.
 */
 _.findIndex = function (ar, callback) {
-	for (var i = ar.length - 1; i >= 0; i--) {
+	for (var i = 0; i < ar.length; i++) {
 		if(callback(ar[i]) === true) {
 			return i;
 		};
 	};
 };
+
+/*
+This method is like _.findIndex except that it iterates over elements of a collection from right to left.
+If a property name is provided for callback the created "_.pluck" style callback will return the property value of the given element.
+ an object is provided for callback the created "_.where" style callback will return true for elements that have the properties of the given object, else false.
+*/
+_.findLastIndex = function (ar, callback) {
+	for (var i = ar.length - 1; i >= 0; i--) {
+		if(callback(ar[i]) === true) {
+			return i; 
+		};
+	};
+};
+
+
 
 module.exports = _;
